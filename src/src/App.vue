4b81dade6713fbb414200bc7cf513e7b873143d0
Michael Mente <mentemichael@live.com>
2022-09-25 22:44:07 +0200

    add new version

---
+++ src/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <ContentPage/>
+</template>
+
+<script>
+import ContentPage from "@/components/ContentPage";
+
+export default {
+  name: 'App',
+  components: {
+    ContentPage
+  }
+}
+</script>
+
+<style>
+@import "./components/styles.css";
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/src/components/ContentPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="wrapper">
+    <div id="main">
+      <div class="inner">
+        <ContainerHead/>
+        <ContainerAbout/>
+        <ContainerSkills/>
+        <ContainerContact/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ContainerHead from "@/components/contents/ContainerHead";
+import ContainerAbout from "@/components/contents/ContainerAbout";
+import ContainerSkills from "@/components/contents/ContainerSkills";
+import ContainerContact from "@/components/contents/ContainerContact";
+
+export default {
+  name: "ContentPage",
+  components: {
+    ContainerContact,
+    ContainerSkills,
+    ContainerAbout,
+    ContainerHead
+  }
+}
+</script>
+
+<style scoped>
+@import "./styles.css";
+</style>
