f11f7a7607d8f5afb92967a70575ecabcb8f5935
Michael Mente <mentemichael@live.com>
2022-09-26 19:07:56 +0200

    add light mode, restructure text

--- portfolio_page/src/components/ContentPage.vue
+++ portfolio_page/src/components/ContentPage.vue
@@ -24,6 +24,9 @@ export default {
     ContainerSkills,
     ContainerAbout,
     ContainerHead
+  },
+  methods: {
+
   }
 }
 </script>
--- portfolio_page/src/components/contents/ContainerAbout.vue
+++ portfolio_page/src/components/contents/ContainerAbout.vue
@@ -6,18 +6,28 @@
           <h2 id="text01" class="style1">About</h2>
         </div>
         <div>
-          <p id="text05" class="style2"><span class="p">Hi!<br> I am a curious explorer of
-                                        technology. My special experience spans full stack development, network
-                                        technologies, data analysis and processing as well as large scale high
-                                        performance computing. I am also profound with distributed system engineering,
-                                        cloud computing, web security and reverse engineering.</span><span class="p">I
-                                        am a computer science student pursuing my MS and work as a full stack software
-                                        engineer at Robert Bosch AG.</span><span class="p">Prior to my current
-                                        occupation, I was employed at the university as a research assistant to
-                                        Professor Stefan Schmid. I developed specialized network components in software
-                                        to be integrated into real networks.<br> In my spare time I spend my time on
-                                        programming microcontrollers as well as 3d printing technologies and the
-                                        modeling techniques that go along with it.</span></p>
+          <p id="text05" class="style2">
+            <span class="p">
+              Hi!
+              <br>
+              I am a computer science student pursuing my MS and work as a full stack software engineer at Robert Bosch AG.
+
+            </span>
+            <span class="p">
+              I am a curious explorer of technology. My area of expertise spans full stack development, network technologies,
+              data analysis and processing as well as large scale high performance computing. I am also profound with
+              distributed system engineering, cloud computing, web security and reverse engineering.
+            </span>
+            <span class="p">
+              Prior to my current occupation, I was employed at the university of Vienna as a research assistant to
+              Professor <a href="https://ct.cs.univie.ac.at/team/person/101989/" class="style2" style="text-decoration: none;">Stefan Schmid</a>. I developed specialized network components in software
+              to be integrated into real networks.
+              <br>
+              In my spare time I spend my time on
+              programming microcontrollers as well as 3d printing technologies and the
+              modeling techniques that go along with it.
+            </span>
+          </p>
         </div>
       </div>
     </div>
